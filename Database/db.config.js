e2280e6787ada36c566e6ab2652ed3eadc3842c4
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2024-10-17 15:33:55 +0530

    Update db.config.js

--- Database/db.config.js
+++ Database/db.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://joshi14:IlHL7VxHTvUlLnQG@emailtrack.kll4x.mongodb.net/?retryWrites=true&w=majority&appName=emailtrack');
     console.log("Database Connected");
   } catch (error) {
     console.log("DB connection Failed");
